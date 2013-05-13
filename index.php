dbd6ebeff0e7c553600e0a35b481086fdc85469d
Abhijeet Bajracharya <abhijeet_bajracharya@hotmail.com>
2013-05-13 10:17:01 +0545

    May 13 10:16AM
    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php defined( '_JEXEC' ) or die; 
+
+include_once JPATH_THEMES.'/'.$this->template.'/logic.php'; // load logic.php
+
+?><!doctype html>
+<!--[if IEMobile]><html class="iemobile" lang="<?php echo $this->language; ?>"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="<?php echo $this->language; ?>"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="<?php echo $this->language; ?>"> <![endif]-->
+<!--[if gt IE 8]><!-->  <html class="no-js" lang="<?php echo $this->language; ?>"> <!--<![endif]-->
+
+<head>
+  <jdoc:include type="head" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <link rel="apple-touch-icon-precomposed" href="<?php echo $tpath; ?>/images/apple-touch-icon-57x57-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $tpath; ?>/images/apple-touch-icon-72x72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $tpath; ?>/images/apple-touch-icon-114x114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $tpath; ?>/images/apple-touch-icon-144x144-precomposed.png">
+  <!--[if lte IE 8]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <?php if ($pie==1) : ?>
+      <style> 
+        {behavior:url(<?php echo $tpath; ?>/js/PIE.htc);}
+      </style>
+    <?php endif; ?>
+  <![endif]-->
+</head>
+  
+<body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('page')).' '.$active->alias.' '.$pageclass; ?>">
+  
+  <!-- 
+    YOUR CODE HERE
+  -->
+  <jdoc:include type="modules" name="debug" />
+</body>
+
+</html>
+
---
+++ logic.php
@@ -0,0 +1,124 @@
+<?php defined('_JEXEC') or die;
+
+// variables
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument(); 
+$params = $app->getParams();
+$headdata = $doc->getHeadData();
+$menu = $app->getMenu();
+$active = $app->getMenu()->getActive();
+$pageclass = $params->get('pageclass_sfx');
+$tpath = $this->baseurl.'/templates/'.$this->template;
+
+// parameter
+$modernizr = $this->params->get('modernizr');
+$cssmethod = $this->params->get('cssmethod');
+$lessjs = $this->params->get('lessjs');
+$foundation = $this->params->get('foundation');
+$bootstrap = $this->params->get('bootstrap');
+$fontawesome = $this->params->get('fontawesome');
+$jquery = $this->params->get('jquery');
+$pie = $this->params->get('pie');
+
+// advanced parameter
+if ($app->isSite()) {
+  // disable js
+  if ( $this->params->get('disablejs') ) {
+    $fnjs=$this->params->get('fnjs');
+    if (trim($fnjs) != '') {
+      $filesjs=explode(',', $fnjs);
+      $head = (array) $headdata['scripts'];
+      $newhead = array();         
+      foreach($head as $key => $elm) {
+        $add = true;
+        foreach ($filesjs as $dis) {
+          if (strpos($key,$dis) !== false) {
+            $add=false;
+            break;
+          } 
+        }
+        if ($add) $newhead[$key] = $elm;
+      }
+      $headdata['scripts'] = $newhead;
+    } 
+  } 
+  // disable css
+  if ( $this->params->get('disablecss') ) {
+    $fncss=$this->params->get('fncss');
+    if (trim($fncss) != '') {
+      $filescss=explode(',', $fncss);
+      $head = (array) $headdata['styleSheets'];
+      $newhead = array();         
+      foreach($head as $key => $elm) {
+        $add = true;
+        foreach ($filescss as $dis) {
+          if (strpos($key,$dis) !== false) {
+            $add=false;
+            break;
+          } 
+        }
+        if ($add) $newhead[$key] = $elm;
+      }
+      $headdata['styleSheets'] = $newhead;
+    } 
+  }
+  $doc->setHeadData($headdata); 
+}
+
+// generator tag
+$this->setGenerator(null);
+
+// force latest IE & chrome frame
+$doc->setMetadata('x-ua-compatible', 'IE=edge,chrome=1');
+
+// add javascripts
+if ($modernizr==1) $doc->addScript($tpath.'/js/modernizr-2.6.2.js');
+
+if ($bootstrap==1 && JVERSION>='3') :
+  JHtml::_('bootstrap.framework');
+elseif ($bootstrap==1) :
+  $doc->addScript($tpath.'/js/jquery-1.9.1.min.js');
+  $doc->addScript($tpath.'/js/jquery-noconflict.js');
+  $doc->addScript($tpath.'/js/bootstrap.min.js');
+endif;
+if ($jquery==1) :
+  $doc->addScript($tpath.'/js/jquery-1.9.1.min.js'); 
+  $doc->addScript($tpath.'/js/main.js');
+endif;
+
+// add stylesheets
+if ($cssmethod=='css') : 
+  if ($foundation==0 && $bootstrap==0) $doc->addStyleSheet($tpath.'/css/normalize.css');
+  
+  if ($bootstrap==1) :
+    $doc->addStyleSheet($tpath.'/css/bootstrap.min.css');
+    $doc->addStyleSheet($tpath.'/css/bootstrap-responsive.min.css');
+    if ($fontawesome==1) $doc->addStyleSheet($tpath.'/css/font-awesome.min.css');
+  endif;
+endif;
+
+// file ending
+if ($cssmethod=='min') : 
+  $ext = '.php'; 
+  $cssmethod = 'css';
+else :
+  $ext = '';
+endif;
+
+// add template sheet
+if ($lessjs==0) $doc->addStyleSheet($tpath.'/'.$cssmethod.'/template.css'.$ext.'?f='.$foundation.'&amp;b='.$bootstrap.'&amp;fa='.$fontawesome.'&amp;v=1');
+if ($lessjs==1 && $cssmethod=='less') :
+  $doc->addCustomTag('<link rel="stylesheet/less" type="text/css" href="'.$tpath.'/'.$cssmethod.'/template.less" />');
+  $doc->addCustomTag('<script src="'.$tpath.'/js/less-1.3.3.min.js" type="text/javascript"></script>');
+  $doc->addCustomTag('<script src="'.$tpath.'/js/browser.js" type="text/javascript"></script>');
+  $doc->addCustomTag('<script type="text/javascript">
+  // <![CDATA[
+    less.env = "development";
+    less.watch();
+  // ]]>
+</script>');
+  //$doc->addScript($tpath.'/js/less-1.3.3.min.js');
+endif;
+
+
+?>
